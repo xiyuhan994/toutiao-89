48be99da1bfd103fe7db7a53067493eb470a1ff3
xiyuhan <xiyuhan@outlook.com>
2019-12-19 21:57:43 +0800

    左侧导航菜单

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,29 @@
 <template>
-<div>
-  主页
-</div>
+  <el-container>
+    <!-- 左侧边栏 -->
+    <el-aside style=" background-color:#353b4e;height:100vh">
+      <layout-aside></layout-aside>
+    </el-aside>
+    <!-- 右侧边栏 -->
+    <el-container>
+      <!-- 头部 -->
+      <el-header>头部</el-header>
+      <!-- 内容部分 -->
+      <el-main>内容</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
+// 左边栏单独写一个组件
+import layoutAside from '../../components/home/layout-aside'
 
+export default {
+  components: {
+    'layout-aside': layoutAside
+  }
 }
 </script>
 
 <style>
-
 </style>
