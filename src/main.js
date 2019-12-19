8364fc1d1d93f05fe81df004af8a6d343d2cbe58
xiyuhan <xiyuhan@outlook.com>
2019-12-19 17:28:22 +0800

    引用axios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 引入element-ui
 import ElementUI from 'element-ui'
 
 import 'element-ui/lib/theme-chalk/index.css'
@@ -9,9 +10,18 @@ import 'element-ui/lib/theme-chalk/index.css'
 // 引入初始化
 import './styles/index.less'
 
+// 引入axios
+import axios from 'axios'
+
 Vue.config.productionTip = false
 
+// 注册事件
 Vue.use(ElementUI)
+// 赋值默认的黑马头条的地址
+axios.defaults.baseURL('http://ttapi.research.itcast.cn/mp/v1_0')
+// 赋值给全局属性
+Vue.prototype.$axios = axios
+Vue.use(axios)
 
 new Vue({
   router,
