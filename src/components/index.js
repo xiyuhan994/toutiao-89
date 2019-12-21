c5d64cdfc51f6f180014c02156a67157ef54ddf8
xiyuhan <xiyuhan@outlook.com>
2019-12-21 22:07:13 +0800

    面包屑组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -4,9 +4,13 @@ import layoutAside from '../components/home/layout-aside'
 // 右侧栏的头部
 import layoutHeader from '../components/home/layout-header'
 
+// 面包屑
+import breadCrumb from './commond/bread-crumd'
+
 export default {
   install (Vue) {
     Vue.component('layout-aside', layoutAside)
     Vue.component('layout-header', layoutHeader)
+    Vue.component('bread-crumb', breadCrumb)
   }
 }
