41d3838c9d8028f1b00c7ab2e85888934320a3db
xiyuhan <xiyuhan@outlook.com>
2019-12-22 21:22:28 +0800

    评论列表-打开和关闭评论

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -6,15 +6,15 @@
     <el-table style="width: 100%" :data="list">
       <!-- 格式化内容 -->
       <el-table-column prop="title" label="标题" width="600"></el-table-column>
-      <el-table-column :formatter="formatterBool"  prop="comment_status" label="评论状态"></el-table-column>
+      <el-table-column :formatter="formatterBool" prop="comment_status" label="评论状态"></el-table-column>
       <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
       <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
       <!-- 文字按钮 -->
       <el-table-column label="操作">
-          <template slot-scope="obj">
+        <template slot-scope="obj">
           <el-button type="text">修改</el-button>
-          <el-button type="text">{{obj.row.comment_status ? '关闭评论':'打开评论'}}</el-button>
-      </template>
+          <el-button type="text" @click='openOnClose(obj.row)'>{{obj.row.comment_status ? '关闭评论':'打开评论'}}</el-button>
+        </template>
       </el-table-column>
     </el-table>
   </el-card>
@@ -41,9 +41,37 @@ export default {
     // 定义一个布尔值的转化方法
     // 当前行数据 当前列属性 当前单元格值 当前下标
     formatterBool (row, column, cellValue, index) {
-      return cellValue ? '正常' : '关上'
+      return cellValue ? '打开' : '关闭'
+    },
+
+    openOnClose (row) {
+      // 获取状态
+      var mess = row.comment_status ? '关闭' : '打开'
+      this.$confirm(`您是否要${mess}评论吗`).then(() => {
+        // 调用接口
+        // 地址参数
+        this.$axios({
+          url: 'comments/status',
+          method: 'put',
+          params: {
+            article_id: row.id
+          },
+          data: {
+            article_comment: !row.comment_status
+          }
+        }).then(result => {
+          // 打开或者关闭评论成功以后
+          this.$message({
+            type: 'success',
+            message: '操作成功'
+          })
+          // 重新请求列表
+          this.getComment()
+        })
+      })
     }
   },
+
   created () {
     this.getComment()
   }
