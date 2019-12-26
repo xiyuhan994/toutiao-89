7278d2f96b4633ad8c80c78b3b60ecd064b933cc
xiyuhan <xiyuhan@outlook.com>
2019-12-26 23:36:04 +0800

    发表文章-频道数据加载

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -19,7 +19,10 @@
               </el-radio-group>
           </el-form-item>
           <el-form-item label='频道'>
-              <el-select></el-select>
+              <el-select>
+                  <!-- //label显示值 -->
+                  <el-option v-for="item in channels" :key="item.id" :value="item.id" :label='item.name'></el-option>
+              </el-select>
           </el-form-item>
           <el-form-item>
               <el-button type='primary'>发布</el-button>
@@ -31,7 +34,26 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      // 定义一个channels接受频道
+      channels: []
+    }
+  },
+  methods: {
+    // 获取频道
+    getChannels () {
+      this.$axios({
+        url: '/channels'
+      }).then(res => {
+        // console.log(res.data.channels)
+        this.channels = res.data.channels
+      })
+    }
+  },
+  created () {
+    this.getChannels()
+  }
 }
 </script>
 
