d6537ace2a181701a9c29243c0eb024f1cdaa6ab
xiyuhan <xiyuhan@outlook.com>
2019-12-29 19:59:24 +0800

    页面进度条引入应用

--- src/permisson/index.js
+++ src/permisson/index.js
@@ -1,8 +1,12 @@
 // 导入
 import router from '../router'
-
+// 进度条
+import progress from 'nprogress'
+import 'nprogress/nprogress.css'
 // 全局前置守卫 每一个路由发生改变之前 触发这个事件
 router.beforeEach(function (to, from, next) {
+  // 开启进度条
+  progress.start()
   if (to.path.startsWith('/home')) {
     let token = window.localStorage.getItem('user-token')
     if (token) {
@@ -13,4 +17,9 @@ router.beforeEach(function (to, from, next) {
   } else {
     next()
   }
+})
+// 加载完成时关闭
+router.afterEach(function () {
+  // 关闭进度条
+  setTimeout(() => progress.done(), 500)
 })
